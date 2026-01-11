105ec6a94030e035e11607221a4a701c8e2432e9
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:21:12 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "vmware_express_route_authorizations" {
+  description = "All vmware_express_route_authorization resources"
+  value       = azurerm_vmware_express_route_authorization.vmware_express_route_authorizations
+}
+
