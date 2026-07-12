679f7d5a70f6862633254df72dc4809bde2a78ec
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,18 @@
-output "vmware_express_route_authorizations" {
-  description = "All vmware_express_route_authorization resources"
-  value       = azurerm_vmware_express_route_authorization.vmware_express_route_authorizations
-  sensitive   = true
-}
 output "vmware_express_route_authorizations_express_route_authorization_id" {
-  description = "List of express_route_authorization_id values across all vmware_express_route_authorizations"
-  value       = [for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : v.express_route_authorization_id]
+  description = "Map of express_route_authorization_id values across all vmware_express_route_authorizations, keyed the same as var.vmware_express_route_authorizations"
+  value       = { for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : k => v.express_route_authorization_id }
 }
 output "vmware_express_route_authorizations_express_route_authorization_key" {
-  description = "List of express_route_authorization_key values across all vmware_express_route_authorizations"
-  value       = [for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : v.express_route_authorization_key]
+  description = "Map of express_route_authorization_key values across all vmware_express_route_authorizations, keyed the same as var.vmware_express_route_authorizations"
+  value       = { for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : k => v.express_route_authorization_key }
   sensitive   = true
 }
 output "vmware_express_route_authorizations_name" {
-  description = "List of name values across all vmware_express_route_authorizations"
-  value       = [for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : v.name]
+  description = "Map of name values across all vmware_express_route_authorizations, keyed the same as var.vmware_express_route_authorizations"
+  value       = { for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : k => v.name }
 }
 output "vmware_express_route_authorizations_private_cloud_id" {
-  description = "List of private_cloud_id values across all vmware_express_route_authorizations"
-  value       = [for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : v.private_cloud_id]
+  description = "Map of private_cloud_id values across all vmware_express_route_authorizations, keyed the same as var.vmware_express_route_authorizations"
+  value       = { for k, v in azurerm_vmware_express_route_authorization.vmware_express_route_authorizations : k => v.private_cloud_id }
 }
 
